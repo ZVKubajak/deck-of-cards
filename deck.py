c8277056e68143c83c9beb8ba07fcc55c502e10d
VanceGoblin <zvkubajak@gmail.com>
2024-10-31 13:53:28 -0500

    Added Recursion to Check for Drawn Cards

--- deck.py
+++ deck.py
@@ -16,57 +16,63 @@ deck = {
     "King of Clubs": (10, False),
     "Ace of Clubs": ([1, 11], False),
 
-    # Diamonds
-    "2 of Diamonds": (2, False),
-    "3 of Diamonds": (3, False),
-    "4 of Diamonds": (4, False),
-    "5 of Diamonds": (5, False),
-    "6 of Diamonds": (6, False),
-    "7 of Diamonds": (7, False),
-    "8 of Diamonds": (8, False),
-    "9 of Diamonds": (9, False),
-    "10 of Diamonds": (10, False),
-    "Jack of Diamonds": (10, False),
-    "Queen of Diamonds": (10, False),
-    "King of Diamonds": (10, False),
-    "Ace of Diamonds": ([1, 11], False),
+    # # Diamonds
+    # "2 of Diamonds": (2, False),
+    # "3 of Diamonds": (3, False),
+    # "4 of Diamonds": (4, False),
+    # "5 of Diamonds": (5, False),
+    # "6 of Diamonds": (6, False),
+    # "7 of Diamonds": (7, False),
+    # "8 of Diamonds": (8, False),
+    # "9 of Diamonds": (9, False),
+    # "10 of Diamonds": (10, False),
+    # "Jack of Diamonds": (10, False),
+    # "Queen of Diamonds": (10, False),
+    # "King of Diamonds": (10, False),
+    # "Ace of Diamonds": ([1, 11], False),
 
-    # Spades
-    "2 of Spades": (2, False),
-    "3 of Spades": (3, False),
-    "4 of Spades": (4, False),
-    "5 of Spades": (5, False),
-    "6 of Spades": (6, False),
-    "7 of Spades": (7, False),
-    "8 of Spades": (8, False),
-    "9 of Spades": (9, False),
-    "10 of Spades": (10, False),
-    "Jack of Spades": (10, False),
-    "Queen of Spades": (10, False),
-    "King of Spades": (10, False),
-    "Ace of Spades": ([1, 11], False),
+    # # Spades
+    # "2 of Spades": (2, False),
+    # "3 of Spades": (3, False),
+    # "4 of Spades": (4, False),
+    # "5 of Spades": (5, False),
+    # "6 of Spades": (6, False),
+    # "7 of Spades": (7, False),
+    # "8 of Spades": (8, False),
+    # "9 of Spades": (9, False),
+    # "10 of Spades": (10, False),
+    # "Jack of Spades": (10, False),
+    # "Queen of Spades": (10, False),
+    # "King of Spades": (10, False),
+    # "Ace of Spades": ([1, 11], False),
 
-    # Hearts
-    "2 of Hearts": (2, False),
-    "3 of Hearts": (3, False),
-    "4 of Hearts": (4, False),
-    "5 of Hearts": (5, False),
-    "6 of Hearts": (6, False),
-    "7 of Hearts": (7, False),
-    "8 of Hearts": (8, False),
-    "9 of Hearts": (9, False),
-    "10 of Hearts": (10, False),
-    "Jack of Hearts": (10, False),
-    "Queen of Hearts": (10, False),
-    "King of Hearts": (10, False),
-    "Ace of Hearts": ([1, 11], False),
+    # # Hearts
+    # "2 of Hearts": (2, False),
+    # "3 of Hearts": (3, False),
+    # "4 of Hearts": (4, False),
+    # "5 of Hearts": (5, False),
+    # "6 of Hearts": (6, False),
+    # "7 of Hearts": (7, False),
+    # "8 of Hearts": (8, False),
+    # "9 of Hearts": (9, False),
+    # "10 of Hearts": (10, False),
+    # "Jack of Hearts": (10, False),
+    # "Queen of Hearts": (10, False),
+    # "King of Hearts": (10, False),
+    # "Ace of Hearts": ([1, 11], False),
 }
 
 
 def draw_card(deck):
     card, (value, has_been_drawn) = random.choice(list(deck.items()))
     # (value = numeric value & has_been_drawn = boolean status)
-    return card, (value, has_been_drawn)
+
+    if not has_been_drawn:
+        deck[card] = (value, True)
+        return card, (value, True)
+    else:
+        print("Test Print")
+        return draw_card(deck)
 
 
 if __name__ == "__main__":
