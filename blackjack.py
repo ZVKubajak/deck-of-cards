89ba034363816fccad803a66a27d7250f5e6eee4
VanceGoblin <zvkubajak@gmail.com>
2024-10-31 19:55:12 -0500

    Defined draw_game_card Function & Created Cards 1 and 2 For User

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,24 @@
-# from deck_standard import deck
+import random
+from deck_standard import deck
+# Due to certain changes in drawing a card during the game.
+# Not importing draw_card function.
+
+
+def draw_game_card(deck):
+    card, (value, has_been_drawn) = random.choice(list(deck.items()))
+
+    if not has_been_drawn:
+        deck[card] = (value, True)
+        return card
+    else:
+        return draw_game_card(deck)
+
 
 if __name__ == "__main__":
+
+    round_count = 0
+    card1 = draw_game_card(deck)
+    card2 = draw_game_card(deck)
 
     while True:
         print("Do you want to play Blackjack? (y/n)")
@@ -15,3 +33,13 @@ if __name__ == "__main__":
             exit()
         else:
             print("Invalid response. Please try again.")
+
+    while True:
+        round_count += 1
+
+        print("")
+        print(f"Round: {round_count}")
+        print("")
+        print(f"Card 1: {card1}")
+        print(f"Card 2: {card2}")
+        break
