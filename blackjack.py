ba637b03ba0e77f89081ea82c6895897a441712c
VanceGoblin <zvkubajak@gmail.com>
2024-10-31 21:38:04 -0500

    Defined find_game_card & calculate_hand_total Functions

--- blackjack.py
+++ blackjack.py
@@ -14,11 +14,40 @@ def draw_game_card(deck):
         return draw_game_card(deck)
 
 
+def find_card_value(total, next_value, aces_count):
+    if next_value in [1, 11]:  # If it's an Ace.
+
+        if total + 11 <= 21:
+            total += 11  # Ace is 11.
+            aces_count += 1
+        else:
+            total += 1  # Ace is 1.
+    else:
+        total += next_value
+
+    # For each Ace that equals 11...
+    # Decrease total by 10 & Ace Count by 1.
+    while total > 21 and aces_count > 0:
+        total -= 10
+        aces_count -= 1
+
+    return total, aces_count
+
+
+def calculate_hand_total(hand):
+    total = 0
+    aces_count = 0
+
+    for card in hand:
+        value, _ = deck[card]
+        total, aces_count = find_card_value(total, value, aces_count)
+
+    return total
+
+
 if __name__ == "__main__":
 
     round_count = 0
-    card1 = draw_game_card(deck)
-    card2 = draw_game_card(deck)
 
     while True:
         print("Do you want to play Blackjack? (y/n)")
@@ -37,9 +66,19 @@ if __name__ == "__main__":
     while True:
         round_count += 1
 
+        p_card1 = draw_game_card(deck)  # Player's 1st Card
+        p_card2 = draw_game_card(deck)  # Player's 2nd Card
+        player_hand = [p_card1, p_card2]  # Player's Hand
+        player_total = calculate_hand_total(player_hand)
+
+        d_card1 = draw_game_card(deck)  # Dealer's 1st Card
+        d_card2 = draw_game_card(deck)  # Dealer's 2nd Card
+        dealer_hand = [d_card1, d_card2]  # Dealer's Hand
+        dealer_total = calculate_hand_total(dealer_hand)
+
         print("")
         print(f"Round: {round_count}")
         print("")
-        print(f"Card 1: {card1}")
-        print(f"Card 2: {card2}")
+
+        # print(f"You have been dealt The {card1} and The {card2}.")
         break
